d0ce0d04734973ef8bb02432954021d58d9c4e69
DIVINEakisa <akisadivine11@gmail.com>
2026-02-25 19:34:41 +0200

    answers

--- classes&OOP/bus.ts
+++ classes&OOP/bus.ts
@@ -1,3 +1,17 @@
-//  Write a TypeScript class called Bus with the properties make, model, and year.
-//  Implement a constructor that initializes these
-//  properties when a Bus object is created.
+class Bus {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year?: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year || 0;
+    console.log(
+      `this is make ${this.make} and model is ${this.model} in ${this.year}`,
+    );
+  }
+}
+
+const car1 = new Bus("Toyota", "Hiace");
+const car2 = new Bus("Mercedes", "Sprinter", 2022);
