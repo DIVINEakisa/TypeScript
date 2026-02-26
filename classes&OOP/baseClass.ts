1c6c105ac07bd69f12ff645367d47fc3d7ff3eef
DIVINEakisa <akisadivine11@gmail.com>
2026-02-26 16:16:02 +0200

    base class

---
+++ classes&OOP/baseClass.ts
@@ -0,0 +1,6 @@
+//  Write a TypeScript class that defines a base class Car with properties
+//   like make, model, and year, along with a start() method that prints a
+//   starting message.Now create a class called Engine with the properties
+//   horsepower and fuelType.Modify the Car class to include an instance of
+//   the Engine class as a property.Implement a method printCarDetails() in
+// the Car class that prints both car and engine details.
