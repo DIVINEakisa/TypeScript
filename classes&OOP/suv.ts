d4e79388df1c839d87464bc086e681d4f8af4273
DIVINEakisa <akisadivine11@gmail.com>
2026-02-26 15:51:17 +0200

    TS class called SUV(Sport Utility Vehicle)

---
+++ classes&OOP/suv.ts
@@ -0,0 +1,4 @@
+// Write a TypeScript class called SUV(Sports Utility Vehicle)
+// that extends the Car class. Add a property to represent
+// whether the SUV is suitable for off - road driving.Implement a method
+// that toggles off - road capability and prints a message accordingly.
