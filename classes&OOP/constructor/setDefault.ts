9d73cda5f2e20fa607fe1322d5ef7519401f3408
DIVINEakisa <akisadivine11@gmail.com>
2026-03-03 15:04:51 +0200

    Set Default value

---
+++ classes&OOP/constructor/setDefault.ts
@@ -0,0 +1,4 @@
+// Write a TypeScript class called Student with properties name and age.
+// Implement a constructor that initializes these properties
+// when a Student object is created.Additionally, add validation to ensure that the age provided is a positive number.
+// If the age is not positive, set it to a default value of 0.
