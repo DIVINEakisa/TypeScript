40ac32050c7b4f574bc05e093751a78739e03037
DIVINEakisa <akisadivine11@gmail.com>
2026-03-07 14:41:25 +0200

    solve all typing issues in the code

---
+++ Gate/rewrite.ts
@@ -0,0 +1,8 @@
+// Solve all the typing issues in the code without changing the implementation
+
+export function concatenate(...strings: string[]): string {
+  let res = strings.join("");
+  return res;
+}
+
+console.log(concatenate("Hello", "Hi", "Hola"));
